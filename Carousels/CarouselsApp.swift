63fa90d7006b6903f6fcff065df3cda4f3aa953c
nshutinicolas <nshuti.nicolas@yahoo.com>
2023-05-08 10:49:48 +0200

    Initial Commit

---
+++ Carousels/CarouselsApp.swift
@@ -0,0 +1,19 @@
+//
+// CarouselsApp.swift
+// Carousels
+//
+// Created by Nicolas Nshuti on 08/05/2023
+// Copyright © 2023 GHOST TECHNOLOGIES LLC. All rights reserved.
+//
+
+
+import SwiftUI
+
+@main
+struct CarouselsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Carousels/ContentView.swift
@@ -0,0 +1,28 @@
+//
+// ContentView.swift
+// Carousels
+//
+// Created by Nicolas Nshuti on 08/05/2023
+// Copyright © 2023 GHOST TECHNOLOGIES LLC. All rights reserved.
+//
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
